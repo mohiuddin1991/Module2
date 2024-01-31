023caa5863bacc8632f2f54d497b843fc8accafa
mohiuddin1991 <146811198+mohiuddin1991@users.noreply.github.com>
2024-01-31 10:03:54 +0530

    Update GeoAltitude.py

--- Codes/Python/GeoAltitude.py
+++ Codes/Python/GeoAltitude.py
@@ -6,7 +6,7 @@
 
 
 import sys                                          #for path to external scripts
-sys.path.insert(0, '/home/VASIREDDY-YASWANTH/github/geometry/codes/CoordGeo')          #path to my scripts
+sys.path.insert(0, './CoordGeo')          #path to my scripts
 import numpy as np
 import numpy.linalg as LA
 import matplotlib.pyplot as plt
@@ -95,7 +95,7 @@ plt.grid() # minor
 plt.axis('equal')
 
 #if using termux
-plt.savefig('figs/triangle/intersect_h.png')
+plt.savefig('intersect_h.png')
 #subprocess.run(shlex.split("termux-open ./figs/tri_sss.pdf"))
 #else
 plt.show()
